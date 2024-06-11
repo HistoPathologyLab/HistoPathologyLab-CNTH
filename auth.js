867993e66a9056538a3ccc9afa89488ebdcb9069
HistoPathologyLab <pathology_lab@hotmail.com>
2024-06-11 11:18:41 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,25 @@
+const { PublicClientApplication } = require('@azure/msal-node');
+
+const msalConfig = {
+  auth: {
+    clientId: process.env.CLIENT_ID,
+    authority: `https://login.microsoftonline.com/${process.env.TENANT_ID}`,
+    clientSecret: process.env.CLIENT_SECRET,
+  },
+};
+
+const pca = new PublicClientApplication(msalConfig);
+
+async function getAccessToken() {
+  try {
+    const authResponse = await pca.acquireTokenByClientCredential({
+      scopes: ['https://graph.microsoft.com/.default'],
+    });
+    return authResponse.accessToken;
+  } catch (error) {
+    console.error('Error acquiring access token:', error);
+    throw error;
+  }
+}
+
+module.exports = { getAccessToken };
