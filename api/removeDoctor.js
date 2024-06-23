97e49c15e5135989a0802336ab8f13747f8b1d24
HistoPathologyLab <pathology_lab@hotmail.com>
2024-06-23 23:29:50 +0530

    updated

--- api/removeDoctor.js
+++ api/removeDoctor.js
@@ -1,13 +1,18 @@
 const axios = require('axios');
 
 async function removeDoctorFromOneDrive(fileName, accessToken) {
-    const url = `https://graph.microsoft.com/v1.0/me/drive/root:/${fileName}`;
+    const url = `https://graph.microsoft.com/v1.0/me/drive/root:${fileName}`;
 
-    await axios.delete(url, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
-    });
+    try {
+        await axios.delete(url, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+    } catch (error) {
+        console.error('Error removing doctor from OneDrive:', error);
+        throw error;
+    }
 }
 
 module.exports = removeDoctorFromOneDrive;
--- api/saveDoctor.js
+++ api/saveDoctor.js
@@ -1,19 +1,24 @@
 const axios = require('axios');
 
 async function saveDoctorToOneDrive(doctor, accessToken) {
+    const folderPath = process.env.ONE_DRIVE_FOLDER_PATH;
     const fileName = `${doctor.name}_${doctor.profession}.json`;
     const fileContent = JSON.stringify(doctor);
 
-    const url = `https://graph.microsoft.com/v1.0/me/drive/root:/${process.env.ONE_DRIVE_FOLDER_PATH}/${fileName}:/content`;
+    const url = `https://graph.microsoft.com/v1.0/me/drive/root:${folderPath}/${fileName}:/content`;
 
-    const response = await axios.put(url, fileContent, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            'Content-Type': 'application/json'
-        }
-    });
-
-    return response.data;
+    try {
+        const response = await axios.put(url, fileContent, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error saving doctor to OneDrive:', error);
+        throw error;
+    }
 }
 
 module.exports = saveDoctorToOneDrive;
