ee8e0aab3a3dd79b652c2dce64fbbc2297f5db5e
HistoPathologyLab <pathology_lab@hotmail.com>
2024-07-04 00:09:16 +0530

    updated cords

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/addregistrarConsultant.html',
+                '/path/to/css/file.css',
+                '/path/to/js/file.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
