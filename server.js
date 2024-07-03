173efe78c5b9b7233f43bc929972f48e35b681b1
HistoPathologyLab <pathology_lab@hotmail.com>
2024-07-03 20:14:10 +0530

    cors and srver

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware setup
+app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,11 +15,15 @@ app.post('/api/saveDoctor', require('./saveDoctor'));
 app.delete('/api/removeDoctor', require('./removeDoctor'));
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the HTML pages
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'addregistrarConsultant.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/addregistrarConsultant.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'addregistrarConsultant.html'));
 });
 
 app.listen(PORT, () => {
