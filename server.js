d91227b0e83dd4f1e1988d2576c93d75a583d90c
HistoPathologyLab <pathology_lab@hotmail.com>
2024-07-31 08:15:49 +0530

    updating

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const { getAccessToken } = require('./getAccessToken'); // Adjust the path if placed in a different folder
+const { getAccessToken } = require('./getAccessToken');
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 
-const saveDoctor = require('./api/saveDoctor'); // Adjust path to include api folder
-const removeDoctor = require('./api/removeDoctor'); // Adjust path to include api folder
+const saveDoctor = require('./api/saveDoctor');
+const removeDoctor = require('./api/removeDoctor');
 
 app.post('/api/saveDoctor', async (req, res) => {
     try {
         const accessToken = await getAccessToken();
         await saveDoctor(req, res, accessToken);
     } catch (error) {
-        console.error('Error in /api/saveDoctor:', error); // Added for debugging
+        console.error('Error in saveDoctor:', error);
         res.status(500).send('Error saving doctor details');
     }
 });
@@ -26,7 +26,7 @@ app.post('/api/removeDoctor', async (req, res) => {
         const accessToken = await getAccessToken();
         await removeDoctor(req, res, accessToken);
     } catch (error) {
-        console.error('Error in /api/removeDoctor:', error); // Added for debugging
+        console.error('Error in removeDoctor:', error);
         res.status(500).send('Error removing doctor details');
     }
 });
