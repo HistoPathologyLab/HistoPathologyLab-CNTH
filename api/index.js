ef91bb4304a48de7d76e754be95e90d86615641b
HistoPathologyLab <pathology_lab@hotmail.com>
2024-08-01 11:18:22 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const { getAccessToken, getAuthCodeUrl } = require('./getAccessToken');
-const saveDoctor = require('./api/saveDoctor');
-const removeDoctor = require('./api/removeDoctor');
+const saveDoctor = require('./saveDoctor');
+const removeDoctor = require('./removeDoctor');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Step 1: Redirect user to the authentication URL
-app.get('/auth', async (req, res) => {
+app.get('/api/auth', async (req, res) => {
     try {
         const authCodeUrl = await getAuthCodeUrl();
         res.redirect(authCodeUrl);
@@ -21,7 +20,7 @@ app.get('/auth', async (req, res) => {
 });
 
 // Step 2: Handle redirect and get access token
-app.get('/auth/callback', async (req, res) => {
+app.get('/api/auth/callback', async (req, res) => {
     const authCode = req.query.code;
 
     try {
@@ -40,6 +39,4 @@ app.post('/api/removeDoctor', async (req, res) => {
     await removeDoctor(req, res, accessToken);
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+module.exports = app;
