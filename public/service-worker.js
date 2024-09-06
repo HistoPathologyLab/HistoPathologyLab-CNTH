3e90dcce4a0439a6bb787cf17c30babe43512b36
PathologyLabRagama <pathology_lab@hotmail.com>
2024-09-06 08:36:37 +0530

    up

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,46 +1,48 @@
-const cacheName = 'v1';
-const cacheAssets = [
+const CACHE_NAME = 'histopathology-lab-cache-v1';
+const urlsToCache = [
+    '/',
     '/index.html',
     '/home.html',
-    '/addregistrarConsultant.html',
     '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/icons/icon-192x192.png', // Updated path to icons
+    '/icons/icon-512x512.png', // Updated path to icons
+    // Add more assets to cache if needed
 ];
 
-// Call Install Event
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches
-            .open(cacheName)
-            .then((cache) => {
-                console.log('Service Worker: Caching Files');
-                return cache.addAll(cacheAssets);
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
             })
-            .then(() => self.skipWaiting())
     );
 });
 
-// Call Activate Event
-self.addEventListener('activate', (e) => {
-    // Remove unwanted caches
-    e.waitUntil(
-        caches.keys().then((cacheNames) => {
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return the response from cache
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== cacheName) {
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
                     }
                 })
             );
         })
-    );
-});
-
-// Call Fetch Event
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
     );
 });
